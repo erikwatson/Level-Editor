dd20880eb8add811f94c89cbc002ab20a1b83943
Erik Watson <evw@gloversure.co.uk>
2019-11-30 17:28:59 +0000

    got webpack working and serving

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,13 @@
 const merge = require('webpack-merge')
-const common = require('webpack.common.js')
+const path = require('path')
+const common = require('./webpack.common.js')
 
-module.exports = merge(common, {})
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dest'),
+    compress: true,
+    port: 9000
+  }
+})
