d9f726deff295bd219f5ebad5b3287b0d56b6045
Erik Watson <evw@gloversure.co.uk>
2019-11-30 18:44:24 +0000

    beginning to style the sidebar

--- src/editor/components/bramble-view/bramble-view.js
+++ src/editor/components/bramble-view/bramble-view.js
@@ -2,5 +2,5 @@ const React = require('react')
 const style = require('./bramble-view.sass')
 
 module.exports = () => {
-  return <div id='bramble-view'>Bramble View</div>
+  return <div id='bramble-view'></div>
 }
