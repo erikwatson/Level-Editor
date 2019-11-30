d04292dc599c79b5b8750510c1fa1ff0d0168811
Erik Watson <evw@gloversure.co.uk>
2019-11-30 18:29:44 +0000

    Getting sass working

--- src/editor/app.js
+++ src/editor/app.js
@@ -1,9 +1,14 @@
 const React = require('react')
+const Sidebar = require('./components/sidebar/sidebar.js')
+const BrambleView = require('./components/bramble-view/bramble-view.js')
+
+const style = require('./app.sass')
 
 module.exports = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div id='app'>
+      <Sidebar />
+      <BrambleView />
     </div>
   )
 }
