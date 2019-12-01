8c9c61448e8ceb635fd31c07e01ede9471e8126c
Erik Watson <evw@gloversure.co.uk>
2019-12-01 21:45:03 +0000

    git reset --hard HEAD

--- src/editor/components/bramble-view/bramble-view.js
+++ src/editor/components/bramble-view/bramble-view.js
@@ -1,6 +1,86 @@
 const React = require('react')
 const style = require('./bramble-view.sass')
 
-module.exports = () => {
-  return <div id='bramble-view'></div>
+const { game, graphics } = require('../../../bramble/src/bramble/bramble')
+
+class BrambleView extends React.Component {
+  componentDidMount() {
+    console.log('bramble view mounted')
+
+    const container = document.querySelector('#bramble-view')
+    game.attachTo(container)
+
+    game.disableContextMenu()
+    game.setSize(this.props.width, this.props.height)
+    game.setSmoothing(false)
+    game.setUpdate(delta => {})
+    game.setRender(() => {
+      graphics.clear('#000000')
+
+      const tl = { x: 0, y: 0 }
+      const tr = { x: this.props.width, y: 0 }
+      const bl = { x: 0, y: this.props.height }
+      const br = { x: this.props.width, y: this.props.height }
+
+      const thickLine = { width: 4, color: '#ffffff' }
+      const midLine = { width: 2, color: '#ff000066' }
+      const thinLine = { width: 1, color: '#ff000066' }
+
+      // Draw the Grid lines
+      for (let i = 0; i < this.props.width / 32; i++) {
+        const top = {
+          x: 32 * i,
+          y: 0
+        }
+
+        const bottom = {
+          x: 32 * i,
+          y: this.props.height
+        }
+
+        if (i % 2 === 0) {
+          graphics.line(top, bottom, thinLine)
+        } else {
+          graphics.line(top, bottom, midLine)
+        }
+      }
+
+      for (let i = 0; i < this.props.height / 32; i++) {
+        const left = {
+          x: 0,
+          y: i * 32
+        }
+
+        const right = {
+          x: this.props.width,
+          y: i * 32
+        }
+
+        if (i % 2 === 0) {
+          graphics.line(left, right, thinLine)
+        } else {
+          graphics.line(left, right, midLine)
+        }
+      }
+
+      // Draw the Bounding Box
+      graphics.line(tl, tr, thickLine)
+      graphics.line(tr, br, thickLine)
+      graphics.line(br, bl, thickLine)
+      graphics.line(bl, tl, thickLine)
+    })
+
+    game.start()
+  }
+
+  componentDidUpdate() {
+    console.log('bramble update')
+    game.setSize(this.props.width, this.props.height)
+  }
+
+  render() {
+    return <div id='bramble-view'></div>
+  }
 }
+
+module.exports = BrambleView
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 const React = require('react')
 const DOM = require('react-dom')
-
+const { Provider } = require('react-redux')
+const store = require('./store')
 const App = require('./editor/app.js')
 
 document.addEventListener('DOMContentLoaded', event => {
-  DOM.render(<App />, document.querySelector('#app-container'))
+  DOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.querySelector('#app-container')
+  )
 })
