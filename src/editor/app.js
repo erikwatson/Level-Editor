944fab5dfc0250840cefabf4a8f7c867893f816f
Erik Watson <championchap@gmail.com>
2019-12-01 21:45:03 +0000

    git reset --hard HEAD

--- src/editor/app.js
+++ src/editor/app.js
@@ -1,14 +1,25 @@
 const React = require('react')
+const { connect } = require('react-redux')
+
 const Sidebar = require('./components/sidebar/sidebar.js')
 const BrambleView = require('./components/bramble-view/bramble-view.js')
 
 const style = require('./app.sass')
 
-module.exports = () => {
+const App = ({ width, height }) => {
   return (
     <div id='app'>
       <Sidebar title='Level Editor' />
-      <BrambleView />
+      <BrambleView width={width} height={height} />
     </div>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    width: state.view.width,
+    height: state.view.height
+  }
+}
+
+module.exports = connect(mapStateToProps)(App)
