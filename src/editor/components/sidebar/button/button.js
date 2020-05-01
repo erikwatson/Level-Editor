328e3a3009e4cc3476fe7aedf8ee507ac59fef1c
Erik Watson <championchap@gmail.com>
2020-05-01 11:59:00 +0100

    Adding tooltips to the tool buttons

--- src/editor/components/sidebar/button/button.js
+++ src/editor/components/sidebar/button/button.js
@@ -1,11 +1,11 @@
 const React = require('react')
 const { FontAwesomeIcon } = require('@fortawesome/react-fontawesome')
 
-module.exports = ({ icon, selected, onClick }) => {
+module.exports = ({ icon, selected, onClick, tip }) => {
   const className = selected ? 'selected' : ''
 
   return (
-    <li className={className} onClick={onClick}>
+    <li className={className} onClick={onClick} title={tip}>
       <FontAwesomeIcon icon={icon} />
     </li>
   )
