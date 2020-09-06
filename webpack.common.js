7c9b3fd36695ebac0f280f6a6ea53eb8a0d483ad
Erik Watson <championchap@gmail.com>
2020-09-06 03:19:43 +0100

    enabling source maps

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/editor-main.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'editor.js',
     path: path.resolve(__dirname, 'dest')
