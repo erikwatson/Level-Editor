050458328b51728e01d82a284b0c3390933a17a2
Erik Watson <e.watson@zengenti.com>
2021-10-23 22:36:36 +0100

    beginning to work on layer controls

--- src/editor/views/map-editor/sidebar/layer-panel/index.tsx
+++ src/editor/views/map-editor/sidebar/layer-panel/index.tsx
@@ -1,12 +1,32 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
+import ButtonGroup from '../../../../components/ui/button-group/button-group'
+import Button from '../../../../components/ui/button/button'
 import Panel from '../../../../components/ui/panel/panel'
 import Layer from './layer'
+import './style.sass'
 
 const LayerPanel = props => {
   return (
     <Panel title='Layers'>
+      <div className='layerControls'>
+        <ButtonGroup>
+          <Button
+            icon={faPlus}
+            selected={false}
+            onClick={e => {}}
+            tip={'Add a Layer'}
+          />
+          <Button
+            icon={faMinus}
+            selected={false}
+            onClick={e => {}}
+            tip={'Remove a Layer'}
+          />
+        </ButtonGroup>
+      </div>
       {props.layers.map((x, i) => {
         const selected = x.position === props.currentLayer ? 'selected' : ''
         const last = i === props.layers.length - 1 ? 'last' : ''
